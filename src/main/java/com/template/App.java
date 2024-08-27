c4144ef25228e450ee8f623fcd735dd5ad9d462a
Tomas <117113921+Masega360@users.noreply.github.com>
2024-08-27 14:48:19 -0300

    Update App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -1,64 +1,104 @@
 package com.template;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.EnumSet.range;
+
 public class App {
 
     // 1. Sum of Two Integers
     public int sum(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        int value = a + b;
+        return value;
     }
 
     // 2. Check Even or Odd
     public boolean isEven(int number) {
-        // TODO: Implement this method
-        return false;
+        if (number % 2 == 0){
+            return false;
+        }
+        return true;
     }
 
     // 3. Maximum of Two Numbers
     public int max(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        int value = 0;
+        if (a > b) {
+            value = a;
+        }
+        if (b > a) {
+            value = b;
+        }
+        return value;
     }
 
     // 4. Factorial of a Number
     public int factorial(int n) {
-        // TODO: Implement this method
-        return 0;
+        int value = 1;
+        for (int num = 1; num <= n; num++) {
+            value *= num;
+        }
+        return value;
     }
 
     // 5. Count Characters in a String
     public int countChars(String input) {
-        // TODO: Implement this method
-        return 0;
+        int length = 0;
+        for (int i = 0; i < input.length(); i++) {
+            length++;
+        }
+        return length;
     }
 
     // 6. Reverse a String
     public String reverse(String input) {
-        // TODO: Implement this method
-        return "";
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+        return reversed;
     }
 
     // 7. Check Prime Number
     public boolean isPrime(int number) {
-        // TODO: Implement this method
-        return false;
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     // 8. Find the Smallest Element in an Array
     public int findMin(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        if (array.length == 0) {
+            return -1;
+        }
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 
     // 9. Sum of Elements in an Array
     public int arraySum(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum;
     }
 
     // 10. Convert Celsius to Fahrenheit
     public double celsiusToFahrenheit(double celsius) {
-        // TODO: Implement this method
-        return 0.0;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        return fahrenheit;
     }
 }
